626d3aa231e3dc81f1fad4aa04c053f1398f6caa
Steph Tyszka <steph@bootsa.net>
2022-03-22 13:08:53 +0100

    fix await for xml output generation issue

--- postprocess.js
+++ postprocess.js
@@ -10,10 +10,12 @@ const json = await readJSON(filename);
 const items = json.results.bindings.map((x) => x.item.value);
 console.log({ items });
 
-items.map((item) => {
+items.map(async (item) => {
     const filename = `./corpus/processed/wikidata-${item.substring(
         item.lastIndexOf("/") + 1,
     )}.xml`;
-    const xml = get(item);
-    writeTXT(filename, xml);
+    const xml = await get(item);
+    console.log(filename);
+    console.log(xml);
+    await writeTXT(filename, xml);
 });
