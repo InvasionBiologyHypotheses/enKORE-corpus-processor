aa2fd84a6ee30bb8ab0b33190af19d3ddd2ac9a4
Steph Tyszka <steph@bootsa.net>
2022-03-23 14:52:56 +0100

    wip: throttle wikidata api calls hacky

--- postprocess.js
+++ postprocess.js
@@ -1,5 +1,7 @@
 import { readJSON, writeTXT } from "https://deno.land/x/flat@0.0.15/mod.ts";
-import { get } from "./lib/xmlexporter.js";
+import citationJS from "https://dev.jspm.io/@citation-js/core";
+import "https://dev.jspm.io/@citation-js/plugin-wikidata";
+import { generateXML } from "./lib/xmlexporter.js";
 
 console.log(Deno.args);
 
@@ -10,20 +12,50 @@ const json = await readJSON(filename);
 const items = json.results.bindings.map((x) => x.item.value);
 console.log({ items });
 
+console.log("citation JS call start");
+const size = 50;
+// const remainder = items.length % size;
+// const runs = (items.length - remainder) / size;
+for (let offset = 0; offset < items.length; offset += size) {
+    for (let i = offset; i < offset + size && items.length; i++) {
+        if (i <= items.length) {
+            console.log({ offset, i });
+            const result = await new citationJS.Cite.async(items[i]);
+            await processItem(result.data[0]);
+            // setTimeout(async () => {
+            //     // console.log(items[i]);
+            //     const result = await new citationJS.Cite.async(items[i]);
+            //     // console.log({ result });
+            //     await processItem(result.data[0]);
+            // }, 10000);
+        }
+    }
+    await sleep(1000);
+}
+console.log("citation JS call stop");
+
+// results.data.map(async (x) => await processItem(x));
+
+async function sleep(time) {
+    await setTimeout(async () => {
+        console.log("sleeping");
+        return null;
+    }, time);
+}
+
 async function processItem(item) {
-    const filename = `./corpus/processed/wikidata-${item.substring(
-        item.lastIndexOf("/") + 1,
-    )}.xml`;
-    const xml = await get(item);
+    console.log({ item });
+    const filename = `./corpus/processed/wikidata-${item.id}.xml`;
+    const xml = await generateXML(item);
     console.log(filename);
     console.log(xml);
     await writeTXT(filename, xml);
 }
 
-for (let i = 100; i < 116; i++) {
-    console.log({ i });
-    processItem(items[i]);
-}
+// for (let i = 0; i < items.length - 1; i++) {
+//     console.log({ i });
+//     processItem(items[i]);
+// }
 // items.map(async (item) => {
 //     await setTimeout(, 500);
 // });
