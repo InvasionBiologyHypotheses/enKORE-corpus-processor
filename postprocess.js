7cd8ec7da39175e2cebe175e47389bf0dd605d17
Steph Tyszka <steph@bootsa.net>
2022-03-31 11:38:50 +0200

    catch unresponsive / errorneous crossref response

--- postprocess.js
+++ postprocess.js
@@ -3,19 +3,12 @@ import citationJS from "https://dev.jspm.io/@citation-js/core";
 import "https://dev.jspm.io/@citation-js/plugin-wikidata";
 import { generateXML } from "./lib/xmlexporter.js";
 
-console.log(Deno.args);
-
 const filename = Deno.args[0];
 const json = await readJSON(filename);
-// console.log({json})
 
 const items = json.results.bindings.map((x) => x.item.value);
-console.log({ items });
 
-console.log("citation JS call start");
 const size = 50;
-// const remainder = items.length % size;
-// const runs = (items.length - remainder) / size;
 for (let offset = 0; offset < items.length; offset += size) {
     for (let i = offset; i < offset + size && items.length; i++) {
         if (i < items.length) {
@@ -24,28 +17,26 @@ for (let offset = 0; offset < items.length; offset += size) {
             const wikidataItem = wdi.data[0];
             let crossrefItem;
             if (wikidataItem.DOI) {
-                crossrefItem = await fetch(
-                    `https://api.crossref.org/works/${encodeURIComponent(
-                        wikidataItem.DOI,
-                    )}`,
-                );
+                crossrefItem = await getCrossrefItem(wikidataItem.DOI);
             }
             await processItem({ wikidataItem, crossrefItem });
-            // setTimeout(async () => {
-            //     // console.log(items[i]);
-            //     const result = await new citationJS.Cite.async(items[i]);
-            //     // console.log({ result });
-            //     await processItem(result.data[0]);
-            // }, 10000);
         }
     }
     await sleep(1000);
 }
-console.log("citation JS call stop");
 
 updateEndpoint();
 
-// results.data.map(async (x) => await processItem(x));
+async function getCrossrefItem(DOI) {
+    let crossrefItem;
+    const crossrefRes = await fetch(
+        `https://api.crossref.org/works/${encodeURIComponent(DOI)}`,
+    );
+    if (crossrefRes.ok) {
+        crossrefItem = (await crossrefRes.json()).message;
+    }
+    return crossrefItem;
+}
 
 async function sleep(time) {
     await setTimeout(async () => {
@@ -55,22 +46,10 @@ async function sleep(time) {
 }
 
 async function processItem({ wikidataItem, crossrefItem }) {
-    console.log({ wikidataItem });
     const filename = `./corpus/processed/wikidata-${wikidataItem.id}.xml`;
-    console.log({ filename });
     const xml = await generateXML({ wikidataItem, crossrefItem });
-    // console.log(filename);
-    // console.log(xml);
     await writeTXT(filename, xml);
 }
-
-// for (let i = 0; i < items.length - 1; i++) {
-//     console.log({ i });
-//     processItem(items[i]);
-// }
-// items.map(async (item) => {
-//     await setTimeout(, 500);
-// });
 
 async function updateEndpoint() {
     const url = `https://enkore.toolforge.org/api/corpus/update.php`;
