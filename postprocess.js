932c32b5efa173ee9abb8f0090e7c442cebf8e61
Steph Tyszka <steph@bootsa.net>
2022-04-13 13:48:55 +0200

    add error checking and import map

--- postprocess.js
+++ postprocess.js
@@ -1,19 +1,32 @@
 import { readJSON, writeTXT } from "https://deno.land/x/flat@0.0.15/mod.ts";
-import citationJS from "https://dev.jspm.io/@citation-js/core";
-import "https://dev.jspm.io/@citation-js/plugin-wikidata";
+import * as citationJS from "@citation-js/core";
+// import "@enkore/citationjs-plugin";
 import { generateXML } from "./lib/xmlexporter.js";
 
-const filename = Deno.args[0];
-const json = await readJSON(filename);
+if (typeof Deno?.args?.[0] === "string") {
+    const filename = Deno.args[0];
+    const json = await readJSON(filename).catch((error) => {
+        console.log(`ERROR: ${error}`);
+        Deno.exit(1);
+    });
 
-await runProcessor(
-    json.results.bindings.map((x) => x.item.value),
-    50,
-    1000,
-).catch((error) => console.log(error));
-const updateRes = await updateEndpoint();
-const notificationRes = await sendNotification(updateRes);
-console.log({ notificationRes });
+    await runProcessor(
+        json.results.bindings.map((x) => x.item.value),
+        50,
+        1000,
+    ).catch((error) => {
+        console.log(`ERROR: ${error}`);
+        Deno.exit(1);
+    });
+    const updateRes = await updateEndpoint();
+    const notificationRes = await sendNotification(updateRes);
+    console.log({ notificationRes });
+} else {
+    console.error(
+        `Filename not passed to postprocessor.js as first argument - ${Deno?.args?.[0]}`,
+    );
+    Deno.exit(1);
+}
 
 async function runProcessor(items) {
     return new Promise(async (resolve) => {
