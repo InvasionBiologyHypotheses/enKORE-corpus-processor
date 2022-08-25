14e6cc40997cc1b1cf4971f76f128a31afcf8aab
Steph Tyszka <steph@bootsa.net>
2022-08-25 14:55:26 +0200

    get and include abstracts Fixes #17 Add abstracts from other sources

--- postprocess.js
+++ postprocess.js
@@ -2,11 +2,71 @@ import { readJSON, writeTXT } from "https://deno.land/x/flat@0.0.15/mod.ts";
 import * as citationJS from "@citation-js/core";
 import "@enkore/citationjs-plugin";
 import { generateXML } from "./lib/xmlexporter.js";
+import { get } from "lodash";
 
+const abstractSources = [
+    {
+        name: "crossref",
+        wikidataProperty: {
+            id: "P356",
+            label: "DOI",
+        },
+        url: (id) => `https://api.crossref.org/v1/works/${id}`,
+        path: "message.abstract",
+        format: "json",
+    },
+    {
+        name: "pmc",
+        wikidataProperty: {
+            id: "P932",
+            label: "PMCID",
+        },
+        url: (id) =>
+            `https://www.ebi.ac.uk/europepmc/webservices/rest/article/PMC/PMC${id}?resultType=core&format=json`,
+        path: "result.abstractText",
+        format: "json",
+    },
+    {
+        name: "pubmed",
+        wikidataProperty: {
+            id: "P698",
+            label: "PMID",
+        },
+        url: (id) =>
+            `https://www.ebi.ac.uk/europepmc/webservices/rest/article/MED/${id}?resultType=core&format=json`,
+        path: "result.abstractText",
+        format: "json",
+    },
+];
 const sleep = (time = 0) => new Promise((resolve) => setTimeout(resolve, time));
+
+async function getAbstract(src, service) {
+    const id = src[service.wikidataProperty.label];
+    // console.log({ id });
+    const url = service.url(id);
+    console.log({ url });
+    const res = await fetch(url);
+    const data = await res.json();
+    // console.log({ data });
+    const out = get(data, service.path);
+    // console.log({ out });
+    return out;
+}
+
+async function findAbstract(wikidataItem) {
+    for (const source of abstractSources) {
+        const foundAbstract = await getAbstract(wikidataItem, source);
+        if (foundAbstract) {
+            console.log(`found abstract in ${source.name}`);
+            return foundAbstract;
+        }
+    }
+    return null;
+}
 
 async function getWikidataItem(entity) {
     const wdi = await new citationJS.Cite.async(entity);
+    console.log(wdi.data[0]);
     return wdi.data[0];
 }
 async function getCrossrefItem(DOI) {
@@ -20,10 +80,14 @@ async function getCrossrefItem(DOI) {
     return crossrefItem;
 }
 
-async function processItem({ wikidataItem, crossrefItem }) {
+async function processItem({ wikidataItem, crossrefItem, accumulatedData }) {
     const filename = `./corpus/processed/wikidata-${wikidataItem.id}.xml`;
-    const xml = await generateXML({ wikidataItem, crossrefItem });
-    await writeTXT(filename, xml);
+    const xml = await generateXML({
+        wikidataItem,
+        crossrefItem,
+        accumulatedData,
+    });
+    return await writeTXT(filename, xml);
 }
 
 async function updateEndpoint() {
@@ -45,6 +109,7 @@ async function updateEndpoint() {
 }
 
 async function main() {
+    console.log("starting main");
     let entries;
 
     if (typeof Deno?.args?.[0] === "string") {
@@ -63,11 +128,21 @@ async function main() {
             if (i < items.length) {
                 console.log({ offset, i });
                 const wikidataItem = await getWikidataItem(items[i]);
+                console.log("wikidataitem id: ", wikidataItem["id"]);
                 let crossrefItem;
                 if (wikidataItem.DOI) {
                     crossrefItem = await getCrossrefItem(wikidataItem.DOI);
                 }
-                await processItem({ wikidataItem, crossrefItem });
+                const accumulatedData = {
+                    abstract: await findAbstract(wikidataItem),
+                };
+                console.log({ accumulatedData });
+                const process = await processItem({
+                    wikidataItem,
+                    crossrefItem,
+                    accumulatedData,
+                });
+                console.log({ process });
             }
         }
         await sleep(1000);
