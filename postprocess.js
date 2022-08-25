1699afe5967e9a884e838f35a10f5a261ce70930
Steph Tyszka <steph@bootsa.net>
2022-08-25 15:01:20 +0200

    chore: update formatting
    too much nesting for 4 spaces indentation - changing to 2

--- postprocess.js
+++ postprocess.js
@@ -5,150 +5,150 @@ import { generateXML } from "./lib/xmlexporter.js";
 import { get } from "lodash";
 
 const abstractSources = [
-    {
-        name: "crossref",
-        wikidataProperty: {
-            id: "P356",
-            label: "DOI",
-        },
-        url: (id) => `https://api.crossref.org/v1/works/${id}`,
-        path: "message.abstract",
-        format: "json",
+  {
+    name: "crossref",
+    wikidataProperty: {
+      id: "P356",
+      label: "DOI",
     },
-    {
-        name: "pmc",
-        wikidataProperty: {
-            id: "P932",
-            label: "PMCID",
-        },
-        url: (id) =>
-            `https://www.ebi.ac.uk/europepmc/webservices/rest/article/PMC/PMC${id}?resultType=core&format=json`,
-        path: "result.abstractText",
-        format: "json",
+    url: (id) => `https://api.crossref.org/v1/works/${id}`,
+    path: "message.abstract",
+    format: "json",
+  },
+  {
+    name: "pmc",
+    wikidataProperty: {
+      id: "P932",
+      label: "PMCID",
     },
-    {
-        name: "pubmed",
-        wikidataProperty: {
-            id: "P698",
-            label: "PMID",
-        },
-        url: (id) =>
-            `https://www.ebi.ac.uk/europepmc/webservices/rest/article/MED/${id}?resultType=core&format=json`,
-        path: "result.abstractText",
-        format: "json",
+    url: (id) =>
+      `https://www.ebi.ac.uk/europepmc/webservices/rest/article/PMC/PMC${id}?resultType=core&format=json`,
+    path: "result.abstractText",
+    format: "json",
+  },
+  {
+    name: "pubmed",
+    wikidataProperty: {
+      id: "P698",
+      label: "PMID",
     },
+    url: (id) =>
+      `https://www.ebi.ac.uk/europepmc/webservices/rest/article/MED/${id}?resultType=core&format=json`,
+    path: "result.abstractText",
+    format: "json",
+  },
 ];
 const sleep = (time = 0) => new Promise((resolve) => setTimeout(resolve, time));
 
 async function getAbstract(src, service) {
-    const id = src[service.wikidataProperty.label];
-    // console.log({ id });
-    const url = service.url(id);
-    console.log({ url });
-    const res = await fetch(url);
-    const data = await res.json();
-    // console.log({ data });
-    const out = get(data, service.path);
-    // console.log({ out });
-    return out;
+  const id = src[service.wikidataProperty.label];
+  // console.log({ id });
+  const url = service.url(id);
+  console.log({ url });
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log({ data });
+  const out = get(data, service.path);
+  // console.log({ out });
+  return out;
 }
 
 async function findAbstract(wikidataItem) {
-    for (const source of abstractSources) {
-        const foundAbstract = await getAbstract(wikidataItem, source);
-        if (foundAbstract) {
-            console.log(`found abstract in ${source.name}`);
-            return foundAbstract;
-        }
+  for (const source of abstractSources) {
+    const foundAbstract = await getAbstract(wikidataItem, source);
+    if (foundAbstract) {
+      console.log(`found abstract in ${source.name}`);
+      return foundAbstract;
     }
-    return null;
+  }
+  return null;
 }
 
 async function getWikidataItem(entity) {
-    const wdi = await new citationJS.Cite.async(entity);
-    console.log(wdi.data[0]);
-    return wdi.data[0];
+  const wdi = await new citationJS.Cite.async(entity);
+  console.log(wdi.data[0]);
+  return wdi.data[0];
 }
 async function getCrossrefItem(DOI) {
-    let crossrefItem;
-    const crossrefRes = await fetch(
-        `https://api.crossref.org/works/${encodeURIComponent(DOI)}`,
-    );
-    if (crossrefRes.ok) {
-        crossrefItem = (await crossrefRes.json()).message;
-    }
-    return crossrefItem;
+  let crossrefItem;
+  const crossrefRes = await fetch(
+    `https://api.crossref.org/works/${encodeURIComponent(DOI)}`,
+  );
+  if (crossrefRes.ok) {
+    crossrefItem = (await crossrefRes.json()).message;
+  }
+  return crossrefItem;
 }
 
 async function processItem({ wikidataItem, crossrefItem, accumulatedData }) {
-    const filename = `./corpus/processed/wikidata-${wikidataItem.id}.xml`;
-    const xml = await generateXML({
-        wikidataItem,
-        crossrefItem,
-        accumulatedData,
-    });
-    return await writeTXT(filename, xml);
+  const filename = `./corpus/processed/wikidata-${wikidataItem.id}.xml`;
+  const xml = await generateXML({
+    wikidataItem,
+    crossrefItem,
+    accumulatedData,
+  });
+  return await writeTXT(filename, xml);
 }
 
 async function updateEndpoint() {
-    const url = `https://enkore.toolforge.org/api/corpus/update.php`;
-    const response = await fetch(url, {
-        method: "GET",
-    });
-    console.log({ response });
-    const notificationresponse = await fetch(Deno.env.get("notification_url"), {
-        method: "POST",
-        body: message,
-        headers: {
-            Title: "Corpus Update",
-            Priority: 3,
-            Tags: "package",
-        },
-    });
-    console.log({ notificationresponse });
+  const url = `https://enkore.toolforge.org/api/corpus/update.php`;
+  const response = await fetch(url, {
+    method: "GET",
+  });
+  console.log({ response });
+  const notificationresponse = await fetch(Deno.env.get("notification_url"), {
+    method: "POST",
+    body: message,
+    headers: {
+      Title: "Corpus Update",
+      Priority: 3,
+      Tags: "package",
+    },
+  });
+  console.log({ notificationresponse });
 }
 
 async function main() {
-    console.log("starting main");
-    let entries;
+  console.log("starting main");
+  let entries;
 
-    if (typeof Deno?.args?.[0] === "string") {
-        const filename = Deno.args[0];
-        entries = await readJSON(filename).catch((error) => {
-            console.log(`ERROR: ${error}`);
-            Deno.exit(1);
-        });
-    }
+  if (typeof Deno?.args?.[0] === "string") {
+    const filename = Deno.args[0];
+    entries = await readJSON(filename).catch((error) => {
+      console.log(`ERROR: ${error}`);
+      Deno.exit(1);
+    });
+  }
 
-    const items = entries.results.bindings.map((x) => x.item.value);
+  const items = entries.results.bindings.map((x) => x.item.value);
 
-    const size = 50;
-    for (let offset = 0; offset < items.length; offset += size) {
-        for (let i = offset; i < offset + size && items.length; i++) {
-            if (i < items.length) {
-                console.log({ offset, i });
-                const wikidataItem = await getWikidataItem(items[i]);
-                console.log("wikidataitem id: ", wikidataItem["id"]);
-                let crossrefItem;
-                if (wikidataItem.DOI) {
-                    crossrefItem = await getCrossrefItem(wikidataItem.DOI);
-                }
-                const accumulatedData = {
-                    abstract: await findAbstract(wikidataItem),
-                };
-                console.log({ accumulatedData });
-                const process = await processItem({
-                    wikidataItem,
-                    crossrefItem,
-                    accumulatedData,
-                });
-                console.log({ process });
-            }
+  const size = 50;
+  for (let offset = 0; offset < items.length; offset += size) {
+    for (let i = offset; i < offset + size && items.length; i++) {
+      if (i < items.length) {
+        console.log({ offset, i });
+        const wikidataItem = await getWikidataItem(items[i]);
+        console.log("wikidataitem id: ", wikidataItem["id"]);
+        let crossrefItem;
+        if (wikidataItem.DOI) {
+          crossrefItem = await getCrossrefItem(wikidataItem.DOI);
         }
-        await sleep(1000);
+        const accumulatedData = {
+          abstract: await findAbstract(wikidataItem),
+        };
+        console.log({ accumulatedData });
+        const process = await processItem({
+          wikidataItem,
+          crossrefItem,
+          accumulatedData,
+        });
+        console.log({ process });
+      }
     }
+    await sleep(1000);
+  }
 
-    updateEndpoint();
+  updateEndpoint();
 }
 
 main();
